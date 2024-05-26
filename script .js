16d1615253688c9aef9425ae173d82ee9ddb34b7
harshcs118 <harshtyagisiyana@gmail.com>
2024-05-26 12:17:12 +0530

    add functionality to the search button on clicking

--- script .js
+++ script .js
@@ -1,1 +1,10 @@
 const apiKey = 'afb74f18960edf1f35f054ff1666ef17';
+
+document.getElementById('search-button').addEventListener('click', function() {
+    const city = document.getElementById('city-input').value.trim();
+    if (city) {
+        fetchWeatherByCity(city);
+    } else {
+        alert('Please enter a city name.');
+    }
+});
